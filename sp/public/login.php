38e9aa3548b7b881a8d34c557532acc40c77cbcb
rajdeepmurde-netspi <134280522+rajdeepmurde-netspi@users.noreply.github.com>
2025-09-13 17:57:17 +0530

    Update login.php

--- sp/public/login.php
+++ sp/public/login.php
@@ -1,1 +1,13 @@
+<?php
+// SP initiates SAML login
+$relay = 'http://sp:8000/acs.php';
+$idp_metadata = file_get_contents('http://idp:8001/metadata.php');
+// parse metadata to get sso URL (naively)
+if (preg_match('/Location="([^"]+)"/', $idp_metadata, $m)) {
+    $idp_sso = $m[1];
+} else {
+    die("Can't get IdP SSO URL");
+}
+header("Location: $idp_sso?ACS=".urlencode($relay)."&RelayState=foobar");
+exit();
 
