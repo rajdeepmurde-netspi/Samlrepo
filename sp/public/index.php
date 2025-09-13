c3ba3855f43e85bd176238d5069f2c4928698ace
rajdeepmurde-netspi <134280522+rajdeepmurde-netspi@users.noreply.github.com>
2025-09-13 17:58:20 +0530

    Update index.php

--- sp/public/index.php
+++ sp/public/index.php
@@ -1,1 +1,8 @@
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    echo "<a href=\"login.php\">Login via SAML</a>";
+} else {
+    echo "Hello, " . htmlspecialchars($_SESSION['user']);
+}
 
